0cda823e73d662f22c51b81125638de9d9cf3e5f
egerdnc <ege.erdinc23@gmail.com>
2021-06-02 01:30:04 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,5 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 server_script {"@mysql-async/lib/MySQL.lua"}
+client_script {"@redm-rpc/lib.lua"}
+server_script {"@redm-rpc/lib.lua"}
 client_script {'client/cl_*.lua'}
 server_script {'server/sv_*.lua'}
